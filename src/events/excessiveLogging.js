cf8625ed5142cd2e88b37905c52d889ff78b2932
Andrew Jones <andrewjones232@gmail.com>
2021-05-07 12:23:06 -0700

    longer logging for excessive demo

--- src/events/excessiveLogging.js
+++ src/events/excessiveLogging.js
@@ -1,7 +1,7 @@
 aha.on({ event: 'aha-develop.server-invocation-triggers.excessiveLogging' }, () => {
   console.log('-------I AM LOGGING ON THE SERVER!!!!------');
 
-  for (let i = 0; i < 100; i++) {
+  for (let i = 0; i < 200; i++) {
     console.log(i);
   }
 });
