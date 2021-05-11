fafd9f0dcc6ed8c452f15e23f46401f6d6fb1049
Andrew Jones <andrewjones232@gmail.com>
2021-05-11 11:06:10 -0700

    Update excessiveLogging.js

--- src/events/excessiveLogging.js
+++ src/events/excessiveLogging.js
@@ -4,8 +4,9 @@ aha.on({ event: 'aha-develop.server-invocation-triggers.excessiveLogging' }, () 
   // tests logging a big nested object. 
   // with i < 16 this is *just* too big - at 15 it will fit, so you can test what its like 
   // viewing a big untruncated object. 
+
   let obj = {};
-  for (let i = 0; i < 16; i++ ){
+  for (let i = 0; i < 14; i++ ) {
     obj[i] = {};
     for (let j = 0; j <= i; j++) {
       obj[i][j] = {};
@@ -14,13 +15,20 @@ aha.on({ event: 'aha-develop.server-invocation-triggers.excessiveLogging' }, () 
       }
     }
   }
+  
 
   console.log(obj);
-
-  // for testing another kind of excessive logging - too many lines. 
-  /* 
-    for (let i = 0; i < 200; i++) {
-      console.log(i);
+  console.log(JSON.stringify(obj).length);
+  
+   
+  // tests logging a bunch of long lines 
+  for (let i = 0; i < 99; i++) {
+    let string = '';
+    for (let j = 0; j < 500; j++) {
+      string += 'a';
     }
-  */
+    console.log(string);
+  }
+
+
 });
